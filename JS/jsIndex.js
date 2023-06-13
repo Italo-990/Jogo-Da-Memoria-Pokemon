63cc3ab8d1364f4fbc611f298e2d10e136f60b93
Ítalo Maia <italo.ribeiro2605@gmail.com>
2023-06-13 10:35:29 +0000

    Update jsIndex.js

--- JS/jsIndex.js
+++ JS/jsIndex.js
@@ -15,15 +15,18 @@ dificil.addEventListener('click',()=>{
     window.location = "./game.html"
 })
 /*------------------------------------------ANDROID------------------------------------*/
-facil.addEventListener('touchstart',()=>{
+facil.addEventListener('touchstart',(e)=>{
+    e.preventDefault()
     localStorage.setItem('dificuldade',5)
     window.location = "./game.html"
 })
-medio.addEventListener('touchstart',()=>{
+medio.addEventListener('touchstart',(e)=>{
+    e.preventDefault()
     localStorage.setItem('dificuldade',10)
     window.location = "./game.html"
 })
-dificil.addEventListener('touchstart',()=>{
+dificil.addEventListener('touchstart',(e)=>{
+    e.preventDefault()
     localStorage.setItem('dificuldade',15)
     window.location = "./game.html"
 })
